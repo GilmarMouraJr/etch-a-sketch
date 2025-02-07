ee9970f2cafdb89baff25dea668e5d6be6495684
GilmarMouraJr <juzinho-gilmar@hotmail.com>
2025-02-07 20:06:50 -0300

    Adjust sketch div size

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function createDivs() {
     const newDiv = document.createElement("div");
     newDiv.classList.add("sketchDiv");
-    // newDiv.style.border = "1px solid black";
-    // newDiv.style.width = "40px";
-    // newDiv.style.height = "40px";
-
     div.appendChild(newDiv);
 }
 
