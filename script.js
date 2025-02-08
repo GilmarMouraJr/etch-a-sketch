040c9ea5996de510b4116160efc085d915c98b0e
GilmarMouraJr <juzinho-gilmar@hotmail.com>
2025-02-07 21:42:43 -0300

    Add refresh button

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function createDivs() {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("sketchDiv");
-    newDiv.addEventListener("mouseenter", changeDivColor);
-    container.appendChild(newDiv);
+function createDivs(divNumber) {
+    for(let i=0; i<divNumber; i++){
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("sketchDiv");
+        newDiv.addEventListener("mouseenter", changeDivColor);
+        container.appendChild(newDiv);
+    }
 }
 
 function changeDivColor() {
     this.style.backgroundColor = "black";
 }
 
+function eraseDivs() {
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+}
+
 const container = document.getElementById("container");
 let divNumber = 256;
 
-for(let i=0; i<divNumber; i++){
-    createDivs();
-}
+    createDivs(divNumber);
 
+const button = document.getElementById("divPerSide");
+button.addEventListener("click", function (e) {
+
+    divNumber = prompt("How many squares per side?", "16");
+    if(divNumber == null || divNumber == "" || divNumber < 2){
+        alert("Invalid square number.")
+        divNumber = 16;
+    }
+
+    eraseDivs();
+    createDivs(divNumber*divNumber);
+});
